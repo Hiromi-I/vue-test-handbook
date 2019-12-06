f4fe65ee5bad27b79d12d1d01014caa17a1d3c05
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-06 19:56:20 +0900

    権限がある状態のpropsでのテスト

--- tests/unit/SubmitButton.spec.js
+++ tests/unit/SubmitButton.spec.js
@@ -15,4 +15,18 @@ describe("SubmitButton", () => {
     expect(wrapper.find("span").text()).toBe("権限がありません")
     expect(wrapper.find("button").text()).toBe("送信する")
   })
+
+  it("権限がある状態のメッセージを表示する", () => {
+    const msg = "送信する"
+    const isAdmin = true
+    const wrapper = shallowMount(SubmitButton, {
+      propsData: {
+        msg,
+        isAdmin
+      }
+    })
+
+    expect(wrapper.find("span").text()).toBe("管理者権限を実行する")
+    expect(wrapper.find("button").text()).toBe("送信する")
+  })
 })
