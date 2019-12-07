754bfb65c0a2b8e22ec36383cf5b8f587e3a3131
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-07 23:45:17 +0900

    NumberRendererコンポーネントを作成

---
+++ src/components/NumberRenderer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "NumberRenderer",
+  props: {
+    even: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
