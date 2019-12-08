8081198b50e839ef6a2b3092164da71fcfd881eb
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-08 18:20:38 +0900

    FormSubmitterコンポーネントを作成

---
+++ src/components/FormSubmitter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <input v-model="username" data-username>
+      <input type="submit">
+    </form>
+
+    <div class="message" v-if="submitted">
+      {{ username }}さん、お問い合わせ、ありがとうございます。
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormSubmitter",
+  data() {
+    return {
+      username: "",
+      submitted: false
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.submitted = true
+    }
+  }
+}
+</script>
