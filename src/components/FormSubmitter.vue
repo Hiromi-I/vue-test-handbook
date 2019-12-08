4af4a68595f301e824bd53d56080ff20d64d3d97
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-08 18:58:15 +0900

    AJAXコールにも対応

--- src/components/FormSubmitter.vue
+++ src/components/FormSubmitter.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <form @submit.prevent="handleSubmit">
+    <form @submit.prevent="handleSubmitAsync">
       <input v-model="username" data-username>
       <input type="submit">
     </form>
+
+    <!-- <form @submit.prevent="handleSubmit">
+      <input v-model="username" data-username>
+      <input type="submit">
+    </form> -->
 
     <div class="message" v-if="submitted">
       {{ username }}さん、お問い合わせ、ありがとうございます。
@@ -23,6 +28,16 @@ export default {
   methods: {
     handleSubmit() {
       this.submitted = true
+    },
+
+    handleSubmitAsync() {
+      return this.$http.get("/api/v1/register", { username: this.username })
+        .then(() => {
+          this.submitted = true
+        })
+        .catch((e) => {
+          throw Error("Something went wrong", e)
+        })
     }
   }
 }
--- tests/unit/FormSubmitter.spec.js
+++ tests/unit/FormSubmitter.spec.js
@@ -1,12 +1,43 @@
+import flushPromises from "flush-promises"
 import { shallowMount } from "@vue/test-utils"
 import FormSubmitter from "@/components/FormSubmitter.vue"
 
-describe("FormSubmitter", () => {
+let url = ""
+let data = ""
+
+const mockHttp = {
+  get: (_url, _data) => {
+    return new Promise((resolve, reject) => {
+      url = _url
+      data = _data
+      resolve()
+    })
+  }
+}
+
+describe.skip("FormSubmitter", () => {
   it("フォームを更新するとお知らせを表示", () => {
     const wrapper = shallowMount(FormSubmitter)
 
     wrapper.find("[data-username]").setValue("alice")
     wrapper.find("form").trigger("submit.prevent")
+
+    expect(wrapper.find(".message").text()).toBe("aliceさん、お問い合わせ、ありがとうございます。")
+  })
+})
+
+describe("FormSubmitter", () => {
+  it("フォームを更新するとお知らせを表示", async () => {
+    const wrapper = shallowMount(FormSubmitter, {
+      mocks: {
+        $http: mockHttp
+      }
+    })
+
+    wrapper.find("[data-username]").setValue("alice")
+    wrapper.find("form").trigger("submit.prevent")
+
+    await flushPromises()
 
     expect(wrapper.find(".message").text()).toBe("aliceさん、お問い合わせ、ありがとうございます。")
   })
