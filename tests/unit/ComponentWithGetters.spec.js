3c888cfcef22dc871e1d559e66a834a016f2518f
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-17 22:29:28 +0900

    テスト用のVueインスタンスを用意してテスト

---
+++ tests/unit/ComponentWithGetters.spec.js
@@ -0,0 +1,23 @@
+import Vuex from "vuex"
+import { shallowMount, createLocalVue } from "@vue/test-utils"
+import ComponentWithVuex from "@/components/ComponentWithVuex.vue"
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    username: "alice"
+  }
+})
+
+describe("ComponentWithVuex", () => {
+  it("renders a username using a real Vuex store", () => {
+    const wrapper = shallowMount(ComponentWithVuex, { 
+      store, 
+      localVue 
+    })
+
+    expect(wrapper.find(".username").text()).toBe("alice")
+  })
+})
