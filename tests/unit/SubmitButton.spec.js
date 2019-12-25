da4b38b774ee2d7babab2ca03a85013c89639f7b
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-25 20:14:25 +0900

    共通処理をリファクタリング

--- tests/unit/SubmitButton.spec.js
+++ tests/unit/SubmitButton.spec.js
@@ -1,19 +1,21 @@
 import { shallowMount } from "@vue/test-utils";
 import SubmitButton from "@/components/SubmitButton.vue";
 
+const msg = "送信する";
+const factory = propsData => {
+  return shallowMount(SubmitButton, { propsData: { msg, ...propsData } });
+};
+
 describe("SubmitButton", () => {
   it("権限がない状態のメッセージを表示する", () => {
-    const msg = "送信する";
-    const wrapper = shallowMount(SubmitButton, { propsData: { msg } });
+    const wrapper = factory();
 
     expect(wrapper.find("span").text()).toBe("権限がありません");
     expect(wrapper.find("button").text()).toBe("送信する");
   });
 
   it("権限がある状態のメッセージを表示する", () => {
-    const msg = "送信する";
-    const isAdmin = true;
-    const wrapper = shallowMount(SubmitButton, { propsData: { msg, isAdmin } });
+    const wrapper = factory({ isAdmin: true });
 
     expect(wrapper.find("span").text()).toBe("管理者権限を実行する");
     expect(wrapper.find("button").text()).toBe("送信する");
