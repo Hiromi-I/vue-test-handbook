dc775975f157c2ca44000d9f5cc45b6649e10332
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-25 20:08:22 +0900

    propsのテスト

--- tests/unit/SubmitButton.spec.js
+++ tests/unit/SubmitButton.spec.js
@@ -2,7 +2,13 @@ import { shallowMount } from "@vue/test-utils";
 import SubmitButton from "@/components/SubmitButton.vue";
 
 describe("SubmitButton", () => {
-  it("権限がない状態のメッセージを表示する", () => {});
+  it("権限がない状態のメッセージを表示する", () => {
+    const msg = "送信する";
+    const wrapper = shallowMount(SubmitButton, { propsData: { msg } });
+
+    expect(wrapper.find("span").text()).toBe("権限がありません");
+    expect(wrapper.find("button").text()).toBe("送信する");
+  });
 
   it("権限がある状態のメッセージを表示する", () => {});
 });
