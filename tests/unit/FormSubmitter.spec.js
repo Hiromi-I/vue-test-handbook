5dfb7ffdc087e3d57ae34c05a17496b09bce4d4c
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-27 22:48:33 +0900

    フォームのユーザー入力をテスト

--- tests/unit/FormSubmitter.spec.js
+++ tests/unit/FormSubmitter.spec.js
@@ -3,7 +3,16 @@ import { shallowMount } from "@vue/test-utils";
 import FormSubmitter from "@/components/FormSubmitter.vue";
 
 describe.skip("FormSubmitter", () => {
-  it("フォームを更新するとお知らせを表示", () => {});
+  it("フォームを更新するとお知らせを表示", () => {
+    const wrapper = shallowMount(FormSubmitter);
+
+    wrapper.find("[data-username]").setValue("alice");
+    wrapper.find("form").trigger("submit.prevent");
+
+    expect(wrapper.find(".message").text()).toBe(
+      "aliceさん、お問い合わせ、ありがとうございます。"
+    );
+  });
 });
 
 describe("FormSubmitter", () => {
