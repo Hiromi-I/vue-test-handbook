e1ab8a79666816bba3131b911a502d7ffbb61ca1
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-27 23:10:06 +0900

    AJAXコールをテスト

--- tests/unit/FormSubmitter.spec.js
+++ tests/unit/FormSubmitter.spec.js
@@ -2,6 +2,19 @@ import flushPromises from "flush-promises";
 import { shallowMount } from "@vue/test-utils";
 import FormSubmitter from "@/components/FormSubmitter.vue";
 
+let url = "";
+let data = "";
+
+const mockHttp = {
+  get: (_url, _data) => {
+    return new Promise((resolve, _reject) => {
+      url = _url;
+      data = _data;
+      resolve();
+    });
+  }
+};
+
 describe.skip("FormSubmitter", () => {
   it("フォームを更新するとお知らせを表示", () => {
     const wrapper = shallowMount(FormSubmitter);
@@ -16,5 +29,22 @@ describe.skip("FormSubmitter", () => {
 });
 
 describe("FormSubmitter", () => {
-  it("フォームを更新するとお知らせを表示", () => {});
+  it("フォームを更新するとお知らせを表示", async () => {
+    const wrapper = shallowMount(FormSubmitter, {
+      mocks: {
+        $http: mockHttp
+      }
+    });
+
+    wrapper.find("[data-username]").setValue("alice");
+    wrapper.find("form").trigger("submit.prevent");
+
+    await flushPromises();
+
+    expect(wrapper.find(".message").text()).toBe(
+      "aliceさん、お問い合わせ、ありがとうございます。"
+    );
+    expect(url).toBe("/api/v1/register");
+    expect(data).toEqual({ username: "alice" });
+  });
 });
