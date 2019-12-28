0a5b9226127f83e3fae0efe666f8999f907ad0a6
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-28 21:17:53 +0900

    emitted APIでコンポーネントが発生したイベントを確認

--- tests/unit/Emitter.spec.js
+++ tests/unit/Emitter.spec.js
@@ -2,7 +2,13 @@ import { shallowMount } from "@vue/test-utils";
 import Emitter from "@/components/Emitter.vue";
 
 describe("Emitter", () => {
-  it("２つの引数があるイベントを発火する", () => {});
+  it("２つの引数があるイベントを発火する", () => {
+    const wrapper = shallowMount(Emitter);
+
+    wrapper.vm.emitEvent();
+
+    expect(wrapper.emitted().myEvent[0]).toEqual(["name", "password"]);
+  });
 
   it("コンポーネントをレンダーせずにイベントを検証する", () => {});
 });
