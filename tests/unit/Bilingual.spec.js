8b73bfff8e1dab7092eb3f57859bcc8e9ecd1b72
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-29 21:48:20 +0900

    vue-i18nのモックテスト

--- tests/unit/Bilingual.spec.js
+++ tests/unit/Bilingual.spec.js
@@ -2,5 +2,9 @@ import { shallowMount } from "@vue/test-utils";
 import Bilingual from "@/components/Bilingual.vue";
 
 describe("Bilingual", () => {
-  it("renders successfully", () => {});
+  it("renders successfully", () => {
+    const wrapper = shallowMount(Bilingual);
+
+    expect(wrapper.text()).toBe("こんにちは、世界！");
+  });
 });
