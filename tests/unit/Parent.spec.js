48e59d9f2e1135144eee58668739a7b658f9d431
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-31 21:50:13 +0900

    デフォルトではspanが表示されないことを確認

--- tests/unit/Parent.spec.js
+++ tests/unit/Parent.spec.js
@@ -3,7 +3,11 @@ import Parent from "@/components/Parent.vue";
 import Child from "@/components/Child.vue";
 
 describe("Parent", () => {
-  it("does not render a span", () => {});
+  it("does not render a span", () => {
+    const wrapper = shallowMount(Parent);
+
+    expect(wrapper.find("span").isVisible()).toBe(false);
+  });
 
   it("does render a span", () => {});
 
